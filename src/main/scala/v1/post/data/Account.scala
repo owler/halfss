71f0c1620bb05904959166080e2b15f235b983d0
owler <owler@tut.by>
2018-12-16 16:40:20 +0300

    draft

--- src/main/scala/v1/post/data/Account.scala
+++ src/main/scala/v1/post/data/Account.scala
@@ -5,15 +5,14 @@ import play.api.libs.json.Reads._
 import play.api.libs.json._
 
 /**
-  * Created by owler on 8/24/2017.
+  * Created by owler on 12/16/2018.
   */
 final case class Account(id: Int, email: String, fname: String, sname: String, phone: String, sex: String, birth: Int, country: String, city: String)
 
-final case class AccountPost(email: Option[String], first_name: Option[String],
-                          last_name: Option[String], gender: Option[String], birth_date: Option[Int]) {
+final case class AccountPost(email: Option[String], fname: Option[String], sname: Option[String], phone: Option[String], sex: Option[String], birth: Option[Int], country: Option[String], city: Option[String]) {
   def verify: Boolean = {
-    email.map(_.length <= 100).getOrElse(true) && first_name.map(_.length <= 50).getOrElse(true) &&
-      last_name.map(_.length <= 50).getOrElse(true) && gender.map(g => g == "m" || g == "f").getOrElse(true)
+    email.map(_.length <= 100).getOrElse(true) && fname.map(_.length <= 50).getOrElse(true) &&
+      sname.map(_.length <= 50).getOrElse(true) && sex.map(g => g == "m" || g == "f").getOrElse(true)
   }
 }
 
@@ -25,10 +24,13 @@ object Account {
       Json.obj(
         "id" -> post.id,
         "email" -> post.email,
-        "first_name" -> post.first_name,
-        "last_name" -> post.last_name,
-        "gender" -> post.gender,
-        "birth_date" -> post.birth_date
+        "fname" -> post.fname,
+        "sname" -> post.sname,
+        "phone" -> post.phone,
+        "sex" -> post.sex,
+        "birth" -> post.birth,
+        "country" -> post.country,
+        "city" -> post.city
       )
     }
   }
@@ -36,21 +38,26 @@ object Account {
   implicit val implicitRead : Reads[Account] = (
     (JsPath \ "id").read[Int] and
     (JsPath \ "email").read[String] and
-    (JsPath \ "first_name").read[String] and
-    (JsPath \ "last_name").read[String] and
-    (JsPath \ "gender").read[String] and
-    (JsPath \ "birth_date").read[Int]
+    (JsPath \ "fname").read[String] and
+    (JsPath \ "sname").read[String] and
+    (JsPath \ "phone").read[String] and
+    (JsPath \ "sex").read[String] and
+    (JsPath \ "birth").read[Int] and
+    (JsPath \ "country").read[String] and
+    (JsPath \ "city").read[String]
   )(Account.apply _)
 }
 
 object AccountPost {
    implicit val implicitRead : Reads[AccountPost] = (
       (JsPath \ "email").readNullable[String] and
-      (JsPath \ "first_name").readNullable[String] and
-      (JsPath \ "last_name").readNullable[String] and
-      (JsPath \ "gender").readNullable[String] and
-      (JsPath \ "birth_date").readNullable[Int]
+        (JsPath \ "fname").readNullable[String] and
+        (JsPath \ "sname").readNullable[String] and
+        (JsPath \ "phone").readNullable[String] and
+        (JsPath \ "sex").readNullable[String] and
+        (JsPath \ "birth").readNullable[Int] and
+        (JsPath \ "country").readNullable[String] and
+        (JsPath \ "city").readNullable[String]
     )(AccountPost.apply _)
-
 
 }

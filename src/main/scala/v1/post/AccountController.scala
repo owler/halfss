c4c2085d0765320542977889a9164ca26805c113
owler <owler@tut.by>
2018-12-18 21:25:54 +0300

    filter draft

--- src/main/scala/v1/post/AccountController.scala
+++ src/main/scala/v1/post/AccountController.scala
@@ -41,6 +41,25 @@ object Lt extends Mstr {
   override def allowed = List("email", "birth")
 }
 
+object Domain extends Mstr {
+  override def prefix: String = "_domain"
+  override def allowed: List[String] = List("email")
+}
+
+object Any extends Mstr {
+  override def prefix: String = "_any"
+  override def allowed: List[String] = List("fname", "city", "interests")
+}
+
+object Neq extends Mstr {
+  override def prefix: String = "_neq"
+  override def allowed: List[String] = List("status")
+}
+
+object Null extends Mstr {
+  override def prefix: String = "_null"
+  override def allowed: List[String] = List("fname", "sname", "phone", "country", "city", "premium")
+}
 /**
   * Takes HTTP requests and produces JSON.
   */
@@ -54,10 +73,17 @@ class AccountController @Inject()(cc: PostControllerComponents)(implicit ec: Exe
     val limit = request.getQueryString("limit").map(_.toInt)
     val list = request.queryString.filterNot(x => x._1 == "query_id" || x._1 == "limit").map(l => l._1 match {
         case Eq(name) => name + "='" + l._2.head + "'"
+        case Neq(name) => name + "!='" + l._2.head + "'"
+        case Null(name) => name + (l._2.head match {
+          case "0" => " is not null"
+          case "1" => " is null"
+        })
         case Lt(name) if name == "birth" => name + "<" + l._2.head
         case Lt(name) => name + "<'" + l._2.head + "'"
         case Gt(name) if name == "birth" => name + ">" + l._2.head
         case Gt(name) => name + ">'" + l._2.head + "'"
+        case Domain(name) => name + " like '%" + l._2.head + "'"
+        case Any(name) => name + " in (" + l._2.head.split(",").map(s => "'" + s + "'").mkString(",") + ")"
         case _ => null
     })
     if (list.exists(_ == null)) {
